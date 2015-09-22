ec72a84066080d014803ed3c8d5887b91a2b7dcb
91.eahz@gmail.com <EHZ@Ericks-MacBook-Pro.local>
2015-09-22 13:42:56 -0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+// =======================================
+// REQUIRED 
+// =======================================
+
+var gulp           = require('gulp'),
+    uglify         = require('gulp-uglify'),
+    rename         = require('gulp-rename'),
+    plumber        = require('gulp-plumber'),
+    browserSync    = require('browser-sync'),
+    reload         = browserSync.reload,
+    autoprefixer   = require('gulp-autoprefixer'),
+    sass           = require('gulp-sass');
+
+// =======================================
+// SCRIPTS
+// =======================================
+
+// gulp.task('scripts', function(){
+//   gulp.src(['js/**/*.js', '!js/**/*.min.js'])
+      // .pipe(plumber())
+//       .pipe(rename({suffix: '.min'}))
+//       .pipe(uglify())
+//       .pipe(gulp.dest('js'))
+     // .pipe(reload({stream: true}));
+
+// });
+// =======================================
+// STYLES
+// =======================================
+
+gulp.task('styles', function(){
+  gulp.src('scss/**/*.scss')
+      .pipe(plumber())
+      .pipe(sass({
+        outputStyle: 'expanded'
+      }))
+      .pipe(autoprefixer('last 2 versions'))
+      .pipe(gulp.dest('css/'))
+      .pipe(reload({stream: true}));
+
+});
+
+// =======================================
+// HTML
+// =======================================
+
+gulp.task('html', function(){
+  gulp.src('*.html')
+      .pipe(reload({stream: true}));
+
+});
+
+
+
+// =======================================
+// BROWSER-SYNC
+// =======================================
+
+gulp.task('browser-sync', function(){
+  browserSync({
+    server: {
+      baseDir: './'
+    }
+  })
+});
+
+
+
+// =======================================
+// WATCH 
+// =======================================
+
+gulp.task('watch', function(){
+  gulp.watch('js/**/*.js', ['scripts']);
+  gulp.watch('scss/**/*.scss', ['styles']);
+  gulp.watch('*.html', ['html']);
+});
+
+
+
+// =======================================
+// DEFAULT
+// =======================================
+
+// gulp.task('default', ['scripts', 'styles', 'watch']);
+gulp.task('default', ['styles', 'html', 'browser-sync', 'watch']);
---
+++ js/script.js
@@ -0,0 +1,6 @@
+console.log("gsfgsgsfgsfg");
+console.log("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, repellat perspiciatis aliquam tenetur officia reiciendis aut provident tempore magnam doloribus enim ipsam, obcaecati cum iure laborum. Incidunt voluptate, corporis distinctio.");
+console.log("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quo sequi illum optio similique sit reiciendis minima dignissimos amet, laborum quisquam earum delectus voluptatem doloribus deleniti maiores iure eum. Similique.");
+
+var duh = ['adfaf', 'afdfadfa'];
+var carol = 7,000,000,000
