8c0ad9966f419a640b9fa4dd92f3516e17524fa8
91.eahz@gmail.com <EHZ@Ericks-MacBook-Pro.local>
2015-09-29 23:33:37 -0400

    add development icons

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 
-$('.assignment-caption:eq(1)').addClass('highlight-variant--1');
-$('.assignment-caption:eq(2)').addClass('highlight-variant--2');
-$('.assignment-caption:eq(3)').addClass('highlight-variant--3');
+// $('.assignment-caption:eq(1)').addClass('highlight-variant--1');
+// $('.assignment-caption:eq(2)').addClass('highlight-variant--2');
+// $('.assignment-caption:eq(3)').addClass('highlight-variant--3');
+
+$('.assignment-caption').each(function(index, el) {
+  console.log(index);
+  var newClass = 'highlight-variant--' + index;
+  console.log(newClass);
+  $(this).addClass(newClass);
+});
